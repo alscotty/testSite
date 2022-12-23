73f44ab51bde2ddc4cc419334a0282ffa83fdb19
alscotty <alawrencescott@gmail.com>
2022-12-23 10:20:51 -0800

    add fireworks

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-const myFunction = () => {
+const myFunction = async() => {
    let button = document.querySelector("button")
     button.classList.add("error-message");
+
+    let div = document.getElementById("main-div")
+    div.classList.add("pyro");
 }
 
 document.querySelector("button").addEventListener("click", myFunction);
